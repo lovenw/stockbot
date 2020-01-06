a867c1ba61ec4858b3253800891fb22d19168458
CHONYONG LEE <nwchon@gmail.com>
2020-01-06 01:58:31 +0000

    remove parser warning

--- stockbot.py
+++ stockbot.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 import re
 
 #보유주식종목
-my_list = ['INTC', 'AMD']
+my_list = ['INTC', 'AMD', 'TSLA']
 
 #현재 주식 가격 가져오기
 for stock in my_list:
@@ -15,7 +15,7 @@ for stock in my_list:
     req = requests.get(currentPrice_URL)
     html = req.text
 
-    soup = BeautifulSoup(html,'html.parser', from_encoding='utf-8')
+    soup = BeautifulSoup(html,'html.parser')#, from_encoding='utf-8')
     text = soup.find('span',{'class':'Trsdu(0.3s) Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(b)'})
 
     text = str(text)
