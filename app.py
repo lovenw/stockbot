746c0fa07a85ade4e784674bb42eec2c8d0cf274
lovenw <nwchon@gmail.com>
2020-01-10 00:54:56 +0900

    slack test

--- app.py
+++ app.py
@@ -1,14 +1,50 @@
-
-from flask import Flask, request, jsonify
+import json
+from slacker import Slacker
+from flask import Flask, request, make_response, jsonify
 import requests
 from bs4 import BeautifulSoup
 import re
+
+token  = "xoxb-891410806117-894827209382-g5EgS5BES9p772FvHN6MCasf"
+slack = Slacker(token)
 
 app = Flask(__name__)
 
 std = ''
 
 # print(std)
+
+def get_answer():
+
+    return "안녕하세요."
+
+    
+# 이벤트 핸들하는 함수
+
+def event_handler(event_type, slack_event):
+
+    if event_type == "app_mention":
+        channel = slack_event["event"]["channel"]
+        text = get_answer()
+        slack.chat.post_message(channel, text)
+        return make_response("앱 멘션 메시지가 보내졌습니다.", 200, )
+    message = "[%s] 이벤트 핸들러를 찾을 수 없습니다." % event_type
+    return make_response(message, 200, {"X-Slack-No-Retry": 1})
+
+
+
+@app.route("/slack", methods=["GET", "POST"])
+
+def hears():
+
+    slack_event = json.loads(request.data)
+    if "challenge" in slack_event:
+        return make_response(slack_event["challenge"], 200, {"content_type": "application/json"})
+    if "event" in slack_event:
+        event_type = slack_event["event"]["type"]
+        return event_handler(event_type, slack_event)
+    return make_response("슬랙 요청에 이벤트가 없습니다.", 404, {"X-Slack-No-Retry": 1})
+
 
 
 
@@ -32,6 +68,7 @@ def stock_price(std) :
 
 print('hello')
 
+'''
 @app.route('/getmsg/', methods=['GET'])
 def respond():
     # Retrieve the name from url parameter
@@ -73,6 +110,7 @@ def post_something():
         return jsonify({
             "ERROR": "no name found, please send a name."
         })
+'''
 
 # A welcome message to test our server
 @app.route('/')
